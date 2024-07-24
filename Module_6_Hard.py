1a1e781df06093bd687cd9c7b78d60e923ee888d
Misha31536 <e4ik@ya.ru>
2024-07-24 16:43:59 +0700

    2024/07/24

--- Module_6_Hard.py
+++ Module_6_Hard.py
@@ -1,17 +1,16 @@
+from math import pi
 class Figure:
 
     sides_count = 0
 
     def __init__(self,__sides, __color, filled):
-        self.__sides = int(__sides)
-        self.__color = list(__color)
+        self.__sides = int(sides)
+        self.__color = list(color)
         self.filled = bool(filled)
 
 
-    def get_color(self, color):
-        if self.__is_valid_color(color): # если метод проверки цвета True
-            c self.__color
-            return color
+    def get_color(self):
+        return self.__color
 
     def __is_valid_color(self, color):
         flag = []
@@ -23,16 +22,46 @@ class Figure:
                     else:
                         flag.append(False)
             if all(flag):
-                return
+                return True
             else:
                 print("Цвет должен быть написано цифрами и в диапазоне от 0 до 255")
+        else:
+            print("Цвет должен быть задан в формате RGB")
+
+    def set_color(self, color):
+        if self.__is_valid_color(color): # если метод проверки цвета True
+            self.__color = color
+            return self.__color
+
+    def __is_valid_sides(self, *sides):
+        if len(sides) == 1:
+            self.__sides = []
+            for i in range(self.sides_count):
+                self.__sides.append(sides[0])
+                print(self.__sides)
+        if len(sides) == self.sides_count:
+            self.__sides = list(sides)
+            print(self.__sides)
+
+    def get_sides(self):
+        return self.__sides
+
+    def __len__(self):
+        return sum(self.__sides)
 
 
+class Circle(Figure):
 
+    sides_count = 1
 
+    def __init__(self, __sides, __color, filled):
+        super().__init__(__sides, __color, filled)
 
-class Circle():
-    pass
+    def __radius(self):
+        print(self._Figure__sides)
+        r = self._Figure__sides / (2 * pi)
+        return r
+
 
 class Triangle():
     pass
@@ -40,6 +69,9 @@ class Triangle():
 class Cube():
     pass
 
-circle1 = Figure(200, [], 100)
-print(circle1.get_color([200,200,200]))
+circle1 = Circle(200, [200,200,200], 100)
+a = circle1._Circle__radius()
+print(a)
+# print(circle1.get_color([200,200,200]))
+# print(circle1._Figure__is_valid_sides(1, 2, 4))
 
